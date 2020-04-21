e9067a805276e84f37fb8986cefdbd8ff527936d
Matt Reed <matthewreed26@gmail.com>
2020-04-21 13:01:29 -0400

    class component can dynamically change now

--- src/App.js
+++ src/App.js
@@ -3,25 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state={place:'My World'};
+  }
+
+  render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React 
-          </a>
-          <p>
-            Look at my props: {JSON.stringify(this.props)}
-          </p>
+          <p>{this.state.place}</p>
+          <button onClick={()=>this.setState({place: 'Hello World'})}>Change String</button>
         </header>
       </div>
     );
