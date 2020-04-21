1b71bde1f0e9df9195d39193ac4c12e3eab073c3
Matt Reed <matthewreed26@gmail.com>
2020-04-20 22:51:52 -0400

    jsx example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function formatPlace(place) {
+  return place.size + ' ' + place.name;
+}
+
+const aPlace = {
+  size: 'Big',
+  name: 'World'
+};
+
+const element =
+  <h1>Hello, {formatPlace(aPlace)}!</h1>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
