f4b5b49df71e322e5966806777023df614d118b4
Matt Reed <matthewreed26@gmail.com>
2020-04-23 20:46:44 -0400

    added react-router-dom for about page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import ClockApp from './clock-app/ClockApp';
+import About from './about/About';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={place:'My World'};
-  }
-
-  render(){
+function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ClockApp/>
-        </header>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Clocks</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/">
+                <ClockApp />
+              </Route>
+            </Switch>
+          </header>
+        </div>
       </div>
+    </Router>
     );
-  }
 }
 
 export default App;
---
+++ src/about/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>React is my favorite front-end framework!</h2>
+    </div>
+  );
+}
+  
+export default About;
